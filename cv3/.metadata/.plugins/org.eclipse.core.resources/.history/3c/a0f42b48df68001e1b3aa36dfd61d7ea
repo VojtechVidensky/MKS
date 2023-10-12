cfdb28e700cfbff9c4e0c9ab872ad3bb9075ca4f
Vojtech Vidensky <230347@vutbr.cz>
2023-10-12 11:35:09 +0200

    final

---
+++ cv3/.metadata/.plugins/org.eclipse.core.resources/.history/3c/a0f42b48df68001e1b3aa36dfd61d7ea
@@ -0,0 +1,14 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 12, 2023
+ *      Author: xviden07
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+	void sct_init(void);
+	void sct_led(uint32_t value);
+
+#endif /* INC_SCT_H_ */
--- cv3/cv3/Core/Inc/sct.h
+++ cv3/cv3/Core/Inc/sct.h
@@ -8,7 +8,8 @@
 #ifndef INC_SCT_H_
 #define INC_SCT_H_
 
-	void sct_init(void);
-	void sct_led(uint32_t value);
+void sct_init(void);
+void sct_led(uint32_t value);
+void sct_value(uint16_t value);
 
 #endif /* INC_SCT_H_ */
